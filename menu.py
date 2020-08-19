d02bbfbadd73dd7c958ecee6761c654c49637db5
Henrich Offermann <henrich.offermann@gmail.com>
2020-08-19 09:37:47 +0200

    odseknutie alergenov a vypis do txt suboru

--- menu.py
+++ menu.py
@@ -2,11 +2,18 @@ import pandas as pd
 
 excelik = pd.ExcelFile('menucka.xlsx')
 kategorie = excelik.sheet_names
+print('Kategorie su: ', kategorie)
 
-print('kategorie su: \n', kategorie)
+myfile=open('vypis.txt', 'w')
 
 for sheet in kategorie:
-    df=pd.read_excel(excelik, sheet_name=sheet, header=None, index=0)
+    df=pd.read_excel(excelik, sheet_name=sheet, header=None)
     
     for riadok in df.values:
-        print(sheet, '-', riadok)
+        if riadok[0][-1]=='/':     #ak je na konci skutocne /
+            r=riadok[0].rsplit('/', 2)[0]
+        else:                      #ak na konci nie je /, teda ziadne alergeny
+            r=riadok[0]
+        myfile.write(' '.join((sheet, '-', r, '\n')))
+
+myfile.close()
